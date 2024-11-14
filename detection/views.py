39bfbba21e005c5c9a51c2541e89ce502ba27dc8
jsm1306 <sindhu.j1729@gmail.com>
2024-11-14 17:02:51 +0530

    Added Image Feature

--- detection/views.py
+++ detection/views.py
@@ -1,16 +1,15 @@
-# Create your views here.
-
 from django.shortcuts import render
 from django.apps import apps
-from PIL import Image
+from PIL import Image as PilImage
 import numpy as np
+import base64
+from io import BytesIO
 from tensorflow.keras.applications.inception_v3 import preprocess_input
 
 def preprocess_image_django(image_file, target_size=(299, 299)):
-    img = Image.open(image_file).convert("RGB")
+    img = PilImage.open(image_file).convert("RGB")
     img = img.resize(target_size)
     img_array = np.array(img)
-    
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
     return img_array
@@ -18,8 +17,9 @@ def preprocess_image_django(image_file, target_size=(299, 299)):
 def predict_image(request):
     if request.method == "POST" and "image" in request.FILES:
         try:
-            image_file = request.FILES["image"]
-            img_array = preprocess_image_django(image_file)
+            img = PilImage.open(request.FILES["image"]).convert("RGB")
+            
+            img_array = preprocess_image_django(request.FILES["image"])
 
             detection_config = apps.get_app_config('detection')
             model = detection_config.model
@@ -27,16 +27,20 @@ def predict_image(request):
             if model:
                 prediction = model.predict(img_array)[0][0]
                 result = "Malignant" if prediction > 0.5 else "Benign"
-                image_file.seek(0)
+                
+                buffered = BytesIO()
+                img.save(buffered, format="PNG")
+                img_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-                return render(request, "result.html", {"prediction": result, "image": image_file})
+                return render(request, "result.html", {
+                    "prediction": result,
+                    "image_data": img_base64
+                })
             else:
                 return render(request, "result.html", {"error": "Model is not loaded."})
         except Exception as e:
             return render(request, "result.html", {"error": f"Error processing image: {str(e)}"})
 
     return render(request, "result.html")
-
-
 def home(request):
     return render(request, 'home.html') 
