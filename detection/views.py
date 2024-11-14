140419c49a405777603ff8ee749c03db1fb2c691
jsm1306 <sindhu.j1729@gmail.com>
2024-11-14 12:31:29 +0530

    Display Image

--- detection/views.py
+++ detection/views.py
@@ -18,7 +18,8 @@ def preprocess_image_django(image_file, target_size=(299, 299)):
 def predict_image(request):
     if request.method == "POST" and "image" in request.FILES:
         try:
-            img_array = preprocess_image_django(request.FILES["image"])
+            image_file = request.FILES["image"]
+            img_array = preprocess_image_django(image_file)
 
             detection_config = apps.get_app_config('detection')
             model = detection_config.model
@@ -26,8 +27,9 @@ def predict_image(request):
             if model:
                 prediction = model.predict(img_array)[0][0]
                 result = "Malignant" if prediction > 0.5 else "Benign"
+                image_file.seek(0)
 
-                return render(request, "result.html", {"prediction": result})
+                return render(request, "result.html", {"prediction": result, "image": image_file})
             else:
                 return render(request, "result.html", {"error": "Model is not loaded."})
         except Exception as e:
